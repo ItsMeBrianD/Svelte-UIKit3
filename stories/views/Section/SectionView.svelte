5ee684299beea7482ec56320e37b241449f9ec0d
Solomon Himelbloom <2001solomon@gmail.com>
2020-08-09 12:48:50 -0800

    Scrub testing information

--- stories/views/Section/SectionView.svelte
+++ stories/views/Section/SectionView.svelte
@@ -1,12 +1,10 @@
 <script>
     import {Section} from "../../../src";
     export let props = {};
-
-    $: console.log(props)
 </script>
 
 <Section {...props}>
-    <h3>Section Heading Test</h3>
+    <h3>Section Heading</h3>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
 
